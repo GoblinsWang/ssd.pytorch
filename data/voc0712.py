5278dc2aa36f2c4f855899af01e99105a21cac96
GoblinsWang <45566796+GoblinsWang@users.noreply.github.com>
2020-08-02 23:16:46 +0800

    Update voc0712.py

--- data/voc0712.py
+++ data/voc0712.py
@@ -25,7 +25,7 @@ VOC_CLASSES = (  # always index 0
     'sheep', 'sofa', 'train', 'tvmonitor')
 
 # note: if you used our download scripts, this should be right
-VOC_ROOT = osp.join('/root/ssd.pytorch/', "data/VOCdevkit/")
+VOC_ROOT = osp.join('/root/', "data/VOCdevkit/")
 
 
 class VOCAnnotationTransform(object):
